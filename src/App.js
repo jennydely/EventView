ae1bea3cf33bf57a2778b8c00dbeddc8348fa991
jennydely <jenny.berndt@gmail.com>
2020-08-26 17:43:18 +0200

    Installed react

---
+++ src/App.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import './App.css';
+
+export default function App() {
+  return (
+    <div className="App">
+      <header className="App-header">
+       
+      </header>
+    </div>
+  );
+}
+
+
