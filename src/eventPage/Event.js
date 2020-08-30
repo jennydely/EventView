01500ab74df0e3704315cb55fd79f949c33b9279
jennydely <jenny.berndt@gmail.com>
2020-08-30 15:19:52 +0200

    Events sorted by year with yearheadlines

--- src/eventPage/Event.js
+++ src/eventPage/Event.js
@@ -1,20 +1,25 @@
 import React from 'react';
 import styled from 'styled-components/macro'
+import {formatDate} from '../utils/date'
 
 export default function Event({event}) {
 const{eventname, eventlocation, eventdate} = event
+
     return (
         <EventItem>
             <Title>{eventname} - {eventlocation}</Title>
-            <Date>{eventdate}</Date>
+            <Duration>{formatDate(eventdate)}</Duration>
         </EventItem>
     )
 }
 
+
+
 const EventItem = styled.li`
 list-style:none;
-border: 2px solid brown;
-margin:20px 0;
+border: 2px solid #964B00;
+marign: 0;
+margin-bottom:20px;
 padding:4px;
 text-align:center;
 `
@@ -22,6 +27,6 @@ const Title = styled.h2`
 margin: 0;
 font-size: 100%;
 `
-const Date = styled.p`
+const Duration = styled.p`
 margin:0;
 `
--- src/eventPage/EventList.js
+++ src/eventPage/EventList.js
@@ -1,21 +1,41 @@
-import React from 'react';
+import React from 'react'
 import Event from './Event'
 import styled from 'styled-components/macro'
 //import {getEvents} from '../utils/services'
 const jsonEvents = require('../mockDB/events.json')
+const eventArray = jsonEvents.slice()
+eventArray.sort((event1, event2) => event1.eventdate > event2.eventdate)
 
 export default function EventList() {
+    const currentYearString = new Date().getFullYear().toString()
+
+    const availableYears = eventArray.reduce((years, event) => {
+        const eventYear = event.eventdate.slice(0, 4)
+        if (!years.includes(eventYear) && eventYear >= currentYearString) years.push(eventYear)
+        return years
+    }, [currentYearString])
 
     return (
-        <EventContainer>
-            {jsonEvents.map(event =>
-                <Event event={event} key={event.id}></Event>
-            )}
-        </EventContainer>
+        <>
+            {availableYears.map(year => (
+                <EventContainer key={year}>
+                    <EventYearHeadline>Event {year}</EventYearHeadline>
+                    {eventArray.map(event => event.eventdate.slice(0, 4) === year &&
+                        <Event event={event} key={event.id}></Event>
+                    )}
+                </EventContainer>
+            ))}
+        </>
     )
 }
 
 const EventContainer = styled.ul`
-margin: 0;
+margin: 10px;
 padding:0;
+`
+const EventYearHeadline= styled.h2`
+margin: 0;
+margin-bottom: 5px;
+padding:0;
+text-align: left;
 `
