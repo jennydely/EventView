8cc01b047c27c02a5dad9493487ac8318727cdc0
jennydely <jenny.berndt@gmail.com>
2020-09-06 22:42:35 +0200

    Add Router to see packlistpage

--- src/App.js
+++ src/App.js
@@ -1,11 +1,20 @@
 import React from 'react';
+import { BrowserRouter as Router, Switch, Route } from 'react-router-dom'
+import PacklistPage from './PackListPage'
 import EventPage from './EventPage'
 export default function App() {
 
   return (
-    <div className="App">
-    <EventPage ></EventPage>
-    </div>
+    <Router>
+         <Route path="/packlist">
+        <PacklistPage />
+      </Route>
+      <Switch>
+        <Route exact path="/">
+          <EventPage /> 
+        </Route>
+        </Switch>
+       </Router>
   );
 }
 
--- src/PackListPage.js
+++ src/PackListPage.js
@@ -1,9 +1,11 @@
 import React, { useState } from 'react';
+import { NavLink } from 'react-router-dom'
 import useEventForm from './componentsEventPage/useEventForm'
 import usePacklists from './usePacklists';
 
-export default function PackListPage(packlistFilter) {
+export default function PackListPage(packlistCategory) {
     const {packlists} = usePacklists
+    const [packlistFilter, setPacklistFilter] = useState('') 
     const { goBack } = useEventForm()  // Button muss noch umgeschrieben werden
     //packlists.sort((event1, event2) => event1.eventStartDate > event2.eventStartDate)// alphabetisch sortieren
     const filteredPacklists = packlists.filter(packlist => packlistFilter === '' || packlist.includes(packlistFilter) )
@@ -18,7 +20,9 @@ export default function PackListPage(packlistFilter) {
                 </ul>
         </main>
         <footer>
+        <NavLink to="/">
             <button type="button" onClick={goBack}>Back</button>
+        </NavLink>
         </footer>
         </>
     )
