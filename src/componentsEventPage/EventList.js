3a22cc8c62f7e8592e030b4e9c64a5f71e41ac95
jennydely <jenny.berndt@gmail.com>
2020-09-07 16:50:13 +0200

    Add sort function a-z or date

--- src/componentsEventPage/EventList.js
+++ src/componentsEventPage/EventList.js
@@ -2,9 +2,9 @@ import React from 'react'
 import EventItem from './EventItem'
 import styled from 'styled-components/macro'
 
-export default function EventList({ eventArray, categoryFilter}) {
-    eventArray.sort((event1, event2) => event1.eventStartDate > event2.eventStartDate)
-    const filteredEvents = eventArray.filter(event => categoryFilter === 'all' || event.category === categoryFilter)
+export default function EventList({ eventArray, categoryFilter, eventFilter}) {
+    const events = eventFilter === 'a-Z' ? eventArray.sort((event1, event2) => event1.name > event2.name) : eventArray.sort((event1, event2) => event1.eventStartDate > event2.eventStartDate)
+    const filteredEvents = events.filter(event => categoryFilter === 'all' || event.category === categoryFilter)
     const currentYearString = new Date().getFullYear().toString()
     const availableYears = filteredEvents.reduce((years, event) => {
         const eventYear = event.eventStartDate.slice(0, 4)
